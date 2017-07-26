2341874d13dc28bcdbb933d9f341a57f22886174
Nick De Veaux <nrdeveaux@gmail.com>
2017-07-26 11:34:44 -0400

    run yeast with 4 bootstraps

--- yeast_bbsr_workflow_runner.py
+++ yeast_bbsr_workflow_runner.py
@@ -4,7 +4,7 @@ workflow = BBSR_TFA_Workflow()
 # Common configuration parameters
 workflow.input_dir = 'data/yeast'
 workflow.priors_file = "yeast-motif-prior.tsv"
-workflow.num_bootstraps = 2
+workflow.num_bootstraps = 4 
 workflow.delTmax = 110
 workflow.delTmin = 0
 workflow.tau = 45
