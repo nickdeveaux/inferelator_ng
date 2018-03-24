9d5c463d41801cd219405a0c36e91ff0ab04b4ab
Nick De Veaux <nrdeveaux@gmail.com>
2018-03-24 18:35:24 -0400

    runner for the TF cells with knn smoothing

---
+++ dmel_bbsr_workflow_runner_TF_knn.py
@@ -0,0 +1,13 @@
+from inferelator_ng.bbsr_tfa_workflow import BBSR_TFA_Workflow
+workflow = BBSR_TFA_Workflow()
+# Common configuration parameters
+workflow.input_dir = 'data/dmel_TF_knn_experiment/'
+workflow.num_bootstraps = 20
+workflow.delTmax = 110
+workflow.delTmin = 0
+workflow.expression_matrix_file = 'wtJ_TF_cells_k_3.tsv'
+workflow.tf_names_file = 'tf_names_all_in_fly_factor.tsv'
+workflow.tau = 45
+workflow.random_seed = 1
+workflow.priors_file = 'prior_with_tfs_filtered_by_one_percent_expression.tsv'
+workflow.run()
