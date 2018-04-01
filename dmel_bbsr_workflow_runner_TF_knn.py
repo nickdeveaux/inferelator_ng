b849961ffed614e13de6b3e82fbe99fb6eda5255
Nick De Veaux <nrdeveaux@gmail.com>
2018-04-01 16:54:52 -0400

    recent dmel run

--- dmel_bbsr_workflow_runner_TF_knn.py
+++ dmel_bbsr_workflow_runner_TF_knn.py
@@ -2,10 +2,10 @@ from inferelator_ng.bbsr_tfa_workflow import BBSR_TFA_Workflow
 workflow = BBSR_TFA_Workflow()
 # Common configuration parameters
 workflow.input_dir = 'data/dmel_TF_knn_experiment/'
-workflow.num_bootstraps = 20
+workflow.num_bootstraps = 60
 workflow.delTmax = 110
 workflow.delTmin = 0
-workflow.expression_matrix_file = 'wtJ_TF_cells_k_3.tsv'
+workflow.expression_matrix_file = 'combined_wtJ_and_wtF_TF_cells_smoothed_separately_with_k_3.tsv'
 workflow.tf_names_file = 'tf_names_all_in_fly_factor.tsv'
 workflow.tau = 45
 workflow.random_seed = 1
